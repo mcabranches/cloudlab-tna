139703dfcaef53569fd990419adb91dd314d2594
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-10-27 13:14:40 -0600

    Fix bug from translation to bash to sh

--- kube_proxy_logger_scripts/xtables-legacy-multi_logger.sh
+++ kube_proxy_logger_scripts/xtables-legacy-multi_logger.sh
@@ -6,14 +6,14 @@ LOGFILE=/mylogs/xtables-legacy-multi.log
 # Collect the calling file.
 # This is useful because several programs are aliased to xtables-legacy-multi
 myCallingFile=$(/lib/modules/kube_proxy_logger_scripts/mybasename "$0")
-if [[ $myCallingFile == "xtables-legacy-multi" ]]; then
+if [ "$myCallingFile" = "xtables-legacy-multi" ]; then
   myCallingFile=""
 fi
 
 # Log the command line for the operation
 echo "$OPERATION $myCallingFile "$@"" >> $LOGFILE
 
-if [[ $myCallingFile == *"-restore" ]]; then
+if [ $myCallingFile = *"-restore" ]; then
   cat > mystdin.txt
   cat mystdin.txt >> $LOGFILE
 
--- kube_proxy_logger_scripts/xtables-nft-multi_logger.sh
+++ kube_proxy_logger_scripts/xtables-nft-multi_logger.sh
@@ -6,14 +6,14 @@ LOGFILE=/mylogs/xtables-nft-multi.log
 # Collect the calling file.
 # This is useful because several programs are aliased to xtables-nft-multi
 myCallingFile=$(/lib/modules/kube_proxy_logger_scripts/mybasename "$0")
-if [[ $myCallingFile == "xtables-nft-multi" ]]; then
+if [ $myCallingFile = "xtables-nft-multi" ]; then
   myCallingFile=""
 fi
 
 # Log the command line for the operation
 echo "$OPERATION $myCallingFile "$@"" >> $LOGFILE
 
-if [[ $myCallingFile == *"-restore" ]]; then
+if [ $myCallingFile = *"-restore" ]; then
   cat > mystdin.txt
   cat mystdin.txt >> $LOGFILE
 
