cf912f289b162db14c4dade378ce939f5fa1b7f8
mcabranches <mcabranches@users.noreply.github.com>
2023-11-16 20:02:00 -0300

    Update profile.py

--- profile.py
+++ profile.py
@@ -9,7 +9,7 @@ import geni.portal as portal
 import geni.rspec.pg as rspec
 
 BASE_IP = "10.10.1"
-BANDWIDTH = 1000000
+BANDWIDTH = 10000000
 TNA_IMAGE = 'urn:publicid:IDN+emulab.net+image+CUDevOpsFall2018:tnak8s'
 #'urn:publicid:IDN+emulab.net+image+CUDevOpsFall2018:xdptna.node-0'
 
